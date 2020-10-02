e014271e37f433eb28e96f8eef2de4862f681aa4
Aleksander Westergaard Karlsen <aleksawk@stud.ntnu.no>
2020-10-02 13:52:53 +0200

    Initial commit

---
+++ movdb/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    trailingComma: "none",
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    tabs: true,
+    bracketSpacing: false,
+    jsxBracketsSameLine: false,
+    arrowParents: "always"
+  };
