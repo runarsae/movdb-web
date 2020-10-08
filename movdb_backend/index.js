cf1d84dd1e5f66a5ca00095e54f9ce455588586f
Runar Sæther <runarsae@stud.ntnu.no>
2020-10-08 10:20:55 +0200

    Add URL to MongoDB in Apollo server

--- movdb_backend/index.js
+++ movdb_backend/index.js
@@ -1,38 +1,43 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
 // A schema is a collection of type definitions
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+	# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  type counters{
-    _id:String,
-    seq:String
-  }
+	type counters {
+		_id: String
+		seq: String
+	}
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each.
+	# The "Query" type is special: it lists all of the available queries that
+	# clients can execute, along with the return type for each.
 
-  type Query {
-    my_query:[counters]
-    hello: String
-  }
+	type Query {
+		my_query: [counters]
+		hello: String
+	}
 `;
-
 
 const resolvers = {
   Query: {
-    hello: () => {
-      return `hey sup ? `;
-    },
-    my_query: async () => {
-      values = await db.collection('counters').find().toArray().then(res => { return res });
-      return values
-    }
-  }
-};
+		hello: () => {
+			return `hey sup ? `;
+		},
+		my_query: async () => {
+			values = await db
+			.collection("counters")
+			.find()
+			.toArray()
+			.then((res) => {
+				return res;
+			});
 
+			return values;
+		},
+  },
+};
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -41,14 +46,17 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+	console.log(`🚀  Server ready at ${url}`);
 });
 
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://it2810-23.idi.ntnu.no:27017";
+const client = new MongoClient(url, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(function (err) {
-  console.log("MONGOdb connected");
-  db = client.db("users"); //mongodb database name
+	console.log("MONGOdb connected");
+	db = client.db("users"); //mongodb database name
 });
