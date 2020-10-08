5aae1758c4febb7f4baf99929a489d04c600d27e
Aleksander Westergaard Karlsen <aleksawk@stud.ntnu.no>
2020-10-08 10:14:04 +0200

    Add ApolloProvider

--- movdb_frontend/src/index.tsx
+++ movdb_frontend/src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
