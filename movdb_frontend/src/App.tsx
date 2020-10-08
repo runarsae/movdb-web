49bdc23da5826fb5edfee69ed7c307f92da72e6e
Aleksander Westergaard Karlsen <aleksawk@stud.ntnu.no>
2020-10-08 11:47:02 +0200

    test getting data

--- movdb_frontend/src/App.tsx
+++ movdb_frontend/src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const test = gql`
+  query movies {
+      movies{
+        id
+        title
+      }
+    }
+`
+;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(test);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    return (
+      <div>
+        {data}
+      </div>
+    )
 }
 
 export default App;
