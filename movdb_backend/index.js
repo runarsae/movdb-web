feb426dac75f15e3bdf7d52023d0b304e3079b57
Runar Sæther <runarsae@stud.ntnu.no>
2020-10-12 15:17:47 +0200

    #14 Add movie types

--- movdb_backend/index.js
+++ movdb_backend/index.js
@@ -1,36 +1,22 @@
-import { ApolloServer, gql } from "apollo-server";
-import { MongoClient } from "mongodb";
+const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
 	type Movie {
 		_id: ID
-		title: String
-		description: String
-		poster: String
+		imdb_id: String
+		original_title: String
+		overview: String
+		genres: String
+		poster_path: String
 		trailer: String
-	}
-
-	type User {
-		_id: ID
-		username: String
-		password: String
-	}
-
-	type MovieRating {
-		_id: ID
-		user: User
-		movie: Movie
-		rating: Int
+		production_companies: String
+		production_countries: String
+		release_date: String
+		runtime: String
 	}
 
 	type Query {
 		movies: [Movie]
-		users: [User]
-		movieRatings: [MovieRating]
-	}
-
-	type Mutation {
-		
 	}
 `;
 
@@ -39,28 +25,6 @@ const resolvers = {
 		movies: async () => {
 			values = await db
 			.collection("movies")
-			.find()
-			.toArray()
-			.then((res) => {
-				return res;
-			});
-
-			return values;
-		},
-		users: async () => {
-			values = await db
-			.collection("users")
-			.find()
-			.toArray()
-			.then((res) => {
-				return res;
-			});
-
-			return values;
-		},
-		movieRatings: async () => {
-			values = await db
-			.collection("movieRatings")
 			.find()
 			.toArray()
 			.then((res) => {
@@ -80,11 +44,12 @@ const resolvers = {
 // Start with 'node index.js'
 const server = new ApolloServer({ typeDefs, resolvers });
 
+// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
 	console.log(`Server ready at ${url}`);
 });
 
-
+const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://aleksawk:Varmongodb1814@it2810-23.idi.ntnu.no:27017/mytestdatabase";
 const client = new MongoClient(url, {
 	useNewUrlParser: true,
